b13faab3c90eaff9dcf16dca27a7bd7af5564732
Frnkovich <mrfrank01@yandex.ru>
2017-12-07 21:46:18 +0300

    Личный проект: максимум подвижности

--- js/map.js
+++ js/map.js
@@ -3,13 +3,21 @@
 (function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
+  var MAP_PIN_MAIN_WIDTH = 65;
+  var MAP_PIN_MAIN_HEIGHT = 87;
+  var MIN_X = 50;
+  var MAX_X = 1150;
+  var MIN_Y = 150;
+  var MAX_Y = 650;
   var clickedElement = null;
+  var mouseDown = false;
   var map = document.querySelector('.map');
   var mapPinMain = map.querySelector('.map__pin--main');
   var noticeForm = document.querySelector('.notice__form');
   var noticeFormFieldset = noticeForm.querySelectorAll('fieldset');
   var mapPins = map.querySelector('.map__pins');
   var mapCard = map.querySelector('.popup');
+  var inputAddress = noticeForm.querySelector('#address');
 
   var isMapPin = function (classList) {
     return classList.contains('map__pin') && !classList.contains('map__pin--main');
@@ -58,12 +66,54 @@
     }
   };
 
+var onMainPin = function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+   };
+   var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    var newY = mapPinMain.offsetTop - shift.y;
+    var newX = mapPinMain.offsetLeft - shift.x;
+
+    if ((newY <= MAX_Y) && (newY >= MIN_Y)) {
+      mapPinMain.style.top = newY + 'px';
+    };
+    if ((newX <= MAX_X) && (newX >= MIN_X)) {
+      mapPinMain.style.left = newX + 'px';
+   };
+    var inputX = newX + MAP_PIN_MAIN_WIDTH / 2;
+    var inputY = newY + MAP_PIN_MAIN_HEIGHT;
+    inputAddress.value = inputX + ', ' + inputY;
+   };
+
+  var onMouseUp = function (upEvt) {
+   upEvt.preventDefault();
+   window.pin.onMainPinClick(upEvt);
+   document.removeEventListener('mousemove', onMouseMove);
+   document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+};
+
   var renderMap = function () {
     window.pin.renderPins(window.data.getAds);
     window.data.disableFormFields(noticeFormFieldset);
     window.card.renderAd(window.data.getAds[0]);
     var closePopup = map.querySelector('.popup__close');
-    mapPinMain.addEventListener('mouseup', window.pin.onMainPinClick);
+    mapPinMain.addEventListener('mousedown', onMainPin);
     closePopup.addEventListener('click', onClosePopup);
     closePopup.addEventListener('keydown', onClosePopup);
     document.addEventListener('keydown', onMapKeydown);
--- js/pin.js
+++ js/pin.js
@@ -3,16 +3,13 @@
 (function () {
   var MAP_PIN_WIDTH = 46;
   var MAP_PIN_HEIGHT = 64;
-  var MAP_PIN_MAIN_WIDTH = 65;
-  var MAP_PIN_MAIN_HEIGHT = 87;
   var map = document.querySelector('.map');
   var mapPins = map.querySelector('.map__pins');
   var mapTemplate = document.querySelector('template').content;
   var mapPin = mapTemplate.querySelector('.map__pin');
   var noticeForm = document.querySelector('.notice__form');
   var noticeFormFieldset = noticeForm.querySelectorAll('fieldset');
-  var inputAddress = noticeForm.querySelector('#address');
-
+  
   var renderMapPin = function (pin, id) {
     var locationX = pin.location.x - MAP_PIN_WIDTH / 2;
     var locationY = pin.location.y - MAP_PIN_HEIGHT;
@@ -39,6 +36,7 @@
   };
 
   var onMainPinClick = function (evt) {
+    evt.preventDefault();
     var pinClicked = evt.target.classList.contains('map__pin--main');
     var clickedMapPinMain = pinClicked ? evt.target : evt.target.parentElement;
     map.classList.remove('map--faded');
@@ -46,9 +44,6 @@
     window.data.enableFormFields(noticeFormFieldset);
     var mapPinAll = mapPins.querySelectorAll('.map__pin');
     showMapPins(mapPinAll);
-    var y = clickedMapPinMain.offsetTop + MAP_PIN_MAIN_HEIGHT;
-    var x = clickedMapPinMain.offsetLeft + MAP_PIN_MAIN_WIDTH / 2;
-    inputAddress.value = x + ', ' + y;
   };
 
   window.pin = {
